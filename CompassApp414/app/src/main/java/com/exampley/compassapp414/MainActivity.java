bd9f1d1eb031038982d853bd98d1c5f39b6dd197
yuri <dbfl4387@naver.com>
2021-04-20 09:26:15 +0900

    Display Compass

---
+++ CompassApp414/app/src/main/java/com/exampley/compassapp414/MainActivity.java
@@ -0,0 +1,16 @@
+package com.exampley.compassapp414;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
